ac5e45cb9593bafcf434709646b8a387b053b8fe
Alexandre Nascimento <alexandrecenec@gmail.com>
2024-10-08 09:58:43 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Bem vindo ao jogo do número secreto');
+let numeroSecreto = 29;
+let chite = prompt('Escolha um número entre 1 e 10');
+
+if(chute == numeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}
